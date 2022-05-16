408191b4d22c24ef4bb47b8ff3553643264d1d0f
djteiji <djteiji@gmail.com>
2022-05-16 00:59:50 -0700

    database calls

--- server.js
+++ server.js
@@ -22,14 +22,40 @@ const db = mysql.createConnection(
   );
 
 
+// // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+//   // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 
 
 
-
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+//   db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
 
 
 
