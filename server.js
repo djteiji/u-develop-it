073e0d15bcde92f1ba6ba1ca270e7584d1e437cc
djteiji <djteiji@gmail.com>
2022-05-17 01:32:28 -0700

    add api routes

--- server.js
+++ server.js
@@ -54,7 +54,12 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
   
     db.query(sql, params, (err, result) => {
@@ -100,7 +105,11 @@ app.post('/api/candidate', ({ body }, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   
     db.query(sql, (err, rows) => {
       if (err) {
