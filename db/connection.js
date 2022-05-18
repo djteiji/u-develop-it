12193d91e7f6e0410c1a38c30ded1c414b607836
djteiji <djteiji@gmail.com>
2022-05-18 00:36:18 -0700

    create route folders

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password 
+    password: '#1stunner',
+    database: 'election'
+  });
+
+
+
+
+
+
+
+
+
+  module.exports = db;
